34a8db93b59279fd1f717db4dc08e485c1c1a493
dstreff <deanna@sandstonestudio.com>
2019-08-04 16:11:44 -0700

    added mysql to package

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.listen(port, () => {
     console.log('listening on ' + port);
 })
 
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
